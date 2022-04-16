8b19aa6f739c60d9b65f3780e45906290914576d
amendezm <alexis.david.mendez@gmail.com>
2022-04-16 11:09:32 -0400

    Configure styles as an entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-/* css */
-import './styles/style.scss';
 const App = () => <h1>Hola Carlos</h1>
 
 export { App }
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
-
 module.exports = {
   entry: {
     main: "./src/index.js",
+    styles: "./src/styles/index.scss",
   },
   output: {
     filename: "[name].js",
@@ -22,14 +22,7 @@ module.exports = {
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
