a57f67ff69e176dc2c720ad8091ba9b47cf55de8
amendezm <alexis.david.mendez@gmail.com>
2022-04-16 11:03:03 -0400

    Sass integration using webpack

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* css */
+import './styles/style.scss';
 const App = () => <h1>Hola Carlos</h1>
 
 export { App }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
+
 module.exports = {
   entry: {
     main: "./src/index.js",
@@ -18,6 +19,17 @@ module.exports = {
       {
         test: /\.jsx?$/i,
         loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
     ],
   },
