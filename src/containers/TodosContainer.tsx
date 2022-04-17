740830d538d070e95a141dca4f4851eefeef78f9
amendezm <alexis.david.mendez@gmail.com>
2022-04-17 14:23:41 -0400

    Create todos hook

--- src/containers/TodosContainer.tsx
+++ src/containers/TodosContainer.tsx
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import { useTodos } from "../hooks/use-todos";
+import { ITodo } from "../types";
+
+const ENTER_KEY_CODE = "Enter";
 
 const TodosContainer = () => {
+  const { todos, saveTodos, addTodo } = useTodos();
+  const [todoValue, setTodoValue] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(event.target.value);
+  };
+
+  const handleAddTodo = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.code === ENTER_KEY_CODE) {
+      const newTodo: ITodo = { text: todoValue, completed: false };
+      addTodo(newTodo);
+      setTodoValue("");
+    }
+  };
+
   return (
     <div className="todos-container">
       <Header title="Todo" />
+      <input
+        type="text"
+        value={todoValue}
+        onChange={handleChange}
+        onKeyDown={handleAddTodo}
+      />
+      {todos?.map((todoItem, index) => (
+        <div key={index}>{todoItem.text}</div>
+      ))}
     </div>
   );
 };
