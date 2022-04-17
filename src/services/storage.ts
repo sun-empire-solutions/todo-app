1a5dd9d0bcd5046632ae8c308416bebd9e1ba749
amendezm <alexis.david.mendez@gmail.com>
2022-04-17 14:23:26 -0400

    Create storage service

---
+++ src/services/storage.ts
@@ -0,0 +1,20 @@
+import { ITodo } from "../types";
+
+const TODOS_STORAGE_KEY = "TODO_LIST";
+
+const getTodos = (): ITodo[] =>
+  JSON.parse(localStorage.getItem(TODOS_STORAGE_KEY));
+
+const addTodo = (todo: ITodo) => {
+  const currentTodosList = getTodos();
+  localStorage.setItem(
+    TODOS_STORAGE_KEY,
+    JSON.stringify([...currentTodosList, todo])
+  );
+};
+
+const saveTodos = (todos: ITodo[]) => {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(todos));
+};
+
+export { getTodos, addTodo, saveTodos };
