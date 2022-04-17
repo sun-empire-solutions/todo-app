ab10f7f4879f2302b8e7d7c552dc006557dd42c8
amendezm <alexis.david.mendez@gmail.com>
2022-04-17 12:20:30 -0400

    Create Header component

---
+++ src/containers/TodosContainer.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../components/Header";
+
+const TodosContainer = () => {
+  return (
+    <div className="todos-container">
+      <Header title="Todo" />
+    </div>
+  );
+};
+
+export { TodosContainer };
