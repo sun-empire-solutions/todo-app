5f601d50aa3547b0f3eee5e8110ab3ea620d0d74
cteyr123 <cteyr123@gmail.com>
2022-04-19 14:49:42 -0400

    add footer and style theme

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer= ()=>{
+    return (
+        <div className="footer-page">
+            <div className="footer">
+                <p>All</p>
+                <p>Active</p>
+                <p>Completed</p>        
+            </div>
+            <div className="footer-text">
+                <p>Drag and drop to reorder list</p>
+            </div>
+        </div>
+
+    );
+};
+
+export {Footer}; 
