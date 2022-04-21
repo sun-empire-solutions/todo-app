aa84fc8431ff9ed1389746734162e81ee48676b3
cteyr123 <cteyr123@gmail.com>
2022-04-21 10:32:56 -0400

    footer selected add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
+
 const Footer= ()=>{
-    return (
-        <div className="footer-page">
-            <div className="footer">
-                <p>All</p>
-                <p>Active</p>
-                <p>Completed</p>        
-            </div>
-            <div className="footer-text">
-                <p>Drag and drop to reorder list</p>
-            </div>
+    const [state,setState]=useState('');
+    type IFilter="all"|"active"|"completed";
+        
+    const clickAll=(value:IFilter)=>()=>{
+            if(value!=state){
+                setState(value);
+            }else{
+                setState("")
+            }         
+       } 
+return (
+    <div className="footer-page">
+        <div className="footer">
+            <p className={`${state==="all"?"selected":""}`}  onClick={clickAll("all")}>All</p>
+            <p className={`${state==="active"?"selected":""}`}  onClick={clickAll("active")} >Active</p>
+            <p className={`${state==="completed"?"selected":""}`}  onClick={clickAll("completed")}>Completed</p>           
         </div>
+        <div className="footer-text">
+            <p>Drag and drop to reorder list</p>
+        </div>
+    </div>
 
     );
 };
