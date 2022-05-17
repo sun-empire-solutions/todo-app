44c52ba5ba650032cb395f61c6a28e24611599d4
amendezm <alexis.david.mendez@gmail.com>
2022-05-17 19:54:10 -0400

    Create variables for backgrounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import { ThemeSwitcher } from "./components/ThemeSwitcher";
-import { TodosContainer } from "./containers/TodosContainer";
-import { ITheme, ThemeContext } from "./context/ThemeContext";
-import { TodoLayout } from "./layouts/TodoLayout";
-
+import { TodosContainer } from "./containers/TodosContainer"
+import { ITheme, ThemeContext } from "./context/ThemeContext"
+import { TodoLayout } from "./layouts/TodoLayout"
 
 const App = () => {
-  const [theme, setTheme] = useState<ITheme>("light");
+  const [theme, setTheme] = useState<ITheme>("light")
 
   const handleThemeToggle = () => {
-    setTheme((theme) => (theme === "light" ? "dark" : "light"));
-  };
+    setTheme((theme) => (theme === "light" ? "dark" : "light"))
+  }
 
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme: handleThemeToggle }}>
@@ -19,7 +17,7 @@ const App = () => {
         <TodosContainer />
       </TodoLayout>
     </ThemeContext.Provider>
-  );
-};
+  )
+}
 
-export { App };
+export { App }
