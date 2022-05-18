2b85af6a8efb038b21af5a0d09ac859f3b7cb790
amendezm <alexis.david.mendez@gmail.com>
2022-05-17 20:00:15 -0400

    Preload images

--- src/layouts/TodoLayout.tsx
+++ src/layouts/TodoLayout.tsx
@@ -2,9 +2,18 @@ import { ITheme } from "../context/ThemeContext"
 
 const TodoLayout = ({ children, theme }: IProps) => (
   <div className={`layout ${theme}`}>
+    <PreloadImages />
     <div className="top top-background" />
     <div className="bottom bg-color" />
     <div className="content">{children}</div>
+  </div>
+)
+
+const PreloadImages = () => (
+  <div className="preload-images">
+    {[...Array(4)].map((_, index) => (
+      <div className={`bg-${index + 1}`}></div>
+    ))}
   </div>
 )
 
